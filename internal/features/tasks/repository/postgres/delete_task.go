1d920530a738aa55c030a366346761584a03a99e
mirwinli <mirwinli.tech@gmail.com>
2026-04-05 14:42:19 +0300

    fixes

--- internal/features/tasks/repository/postgres/delete_task.go
+++ internal/features/tasks/repository/postgres/delete_task.go
@@ -7,7 +7,7 @@ import (
 	core_errors "github.com/Mirwinli/golang-todoapp/internal/core/errors"
 )
 
-func (r *TasksRepository) DeleteTask(ctx context.Context, id string) error {
+func (r *TasksRepository) DeleteTask(ctx context.Context, id int) error {
 	ctx, cancel := context.WithTimeout(ctx, r.pool.OpTimeout())
 	defer cancel()
 
